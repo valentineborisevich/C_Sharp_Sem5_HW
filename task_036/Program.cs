effabf07c61f94deb9eb5defd0d4038d56302ddb
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-22 18:31:40 +0300

    task 036 finished

--- task_036/Program.cs
+++ task_036/Program.cs
@@ -3,3 +3,29 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 99);
+    }
+    return array;
+}
+
+Console.WriteLine("Enter array's length");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] Arr = new int[length];
+FillArray(Arr);
+Console.WriteLine("Your array is");
+Console.WriteLine(string.Join(", ", Arr));
+Console.WriteLine("Now I'll find sum of elements of even indexes");
+int sum = 0;
+for (int i = 1; i < Arr.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        sum+=Arr[i];
+    }
+}
+Console.WriteLine(sum);
