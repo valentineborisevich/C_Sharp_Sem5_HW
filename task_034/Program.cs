450a40770cf9c5d86d63534f15390c92fface15f
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-22 18:11:47 +0300

    task 034 finished

--- task_034/Program.cs
+++ task_034/Program.cs
@@ -2,3 +2,29 @@
 
 // [345, 897, 568, 234] -> 2
 
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+Console.WriteLine("Enter array's length");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] Arr = new int[length];
+FillArray(Arr);
+Console.WriteLine("Your array is");
+Console.WriteLine(string.Join(", ", Arr));
+Console.WriteLine("Now I'll find quantity of even numbers");
+
+int count = 0;
+for (int i = 0; i < Arr.Length; i++)
+{
+    if (Arr[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
